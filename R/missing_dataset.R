a4142df355a1e00b9c29af8ea8c704d173085c31
dlemas <djlemas@ufl.edu>
2019-02-21 07:42:22 -0700

    missing report: data import

---
+++ R/missing_dataset.R
@@ -0,0 +1,50 @@
+##-------------- 
+# **************************************************************************** #
+# ***************                Project Overview              *************** #
+# **************************************************************************** #
+
+# Author:      Dominick Lemas 
+# Date:        Fedb 21, 2019 
+# IRB:
+# Description: identify missing data for UF waste water
+# Data: C:\Users\djlemas\Dropbox (UFL)\02_Projects\BEACH_STUDY\RedCap
+
+# **************************************************************************** #
+# ***************                Directory Variables           *************** #
+# **************************************************************************** #
+
+# Computer
+location="djlemas";location
+
+# Directory Locations
+
+work.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\WASTE_WATER\\2018_KY_Sep18\\",sep="");work.dir
+data.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\WASTE_WATER\\2018_KY_Sep18\\",sep="");data.dir
+out.dir=paste("C:\\Users\\",location,"\\Dropbox (UFL)\\02_Projects\\WASTE_WATER\\2018_KY_Sep18\\",sep="");out.dir
+
+# Set Working Directory
+setwd(work.dir)
+list.files()
+
+# **************************************************************************** #
+# ***************                Library                       *************** #
+# **************************************************************************** #
+
+# library(readxl)
+# install.packages(data.table)
+library(data.table)
+library(tidyr)
+library(dplyr)
+library(reshape2)
+
+# **************************************************************************** #
+# ***************  TheBreastfeedingAndE_BILLING_DATA_2018-06-08_1318.csv                                              
+# **************************************************************************** # 
+
+#Read Data
+data.file.name="UF_WasteWater_ResultsCleaned.csv";data.file.name
+data.file.path=paste0(data.dir,"",data.file.name);data.file.path
+waste.dat<- read.csv(data.file.path);
+
+# look at data
+head(waste.dat); str(waste.dat); names(waste.dat)
