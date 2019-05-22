89b7c948e81810f2f712b6422cb01e8406ab0171
Dominick J. Lemas <dominicklemas@gmail.com>
2019-05-21 21:07:55 -0400

    explore waste water

--- R/explore_data.R
+++ R/explore_data.R
@@ -12,7 +12,6 @@
 # ***************                Directory Variables           *************** #
 # **************************************************************************** #
 
-
 # Directory Locations
 work.dir=paste0(Sys.getenv("USERPROFILE"),"\\Dropbox (UFL)\\02_Projects\\WASTE_WATER\\2018_KY_Sep18\\");work.dir
 data.dir=paste0(Sys.getenv("USERPROFILE"),"\\Dropbox (UFL)\\02_Projects\\WASTE_WATER\\2018_KY_Sep18\\");data.dir
@@ -32,47 +31,39 @@ list.files()
 library(tidyr)
 library(dplyr)
 library(tidyverse)
+library(ggplot2)
 
 # **************************************************************************** #
-# ***************  UF_WasteWater_ResultsCleaned.csv                                              
+# ***************                  load.RData                                              
 # **************************************************************************** # 
 
 #Read Data
-data.file.name="UF_WasteWater_ResultsCleaned.csv";data.file.name
+data.file.name="water.RData";data.file.name
 data.file.path=paste0(data.dir,"",data.file.name);data.file.path
-waste.dat<- read.csv(data.file.path);
+load(data.file.path);
 
 # look at data
-head(waste.dat); str(waste.dat); names(waste.dat)
+water=water.df
+head(water); str(water); names(water)
 
-# format time
-waste.dat$Location=as.factor(waste.dat$Location)
+# count by location
+time.by.location.count=water %>%
+  group_by(time) %>%
+  count(location)
 
-# format time
-unique(waste.dat$Time)
-levels(waste.dat$Time)
-
-# 11 time points
-waste.dat$Time = factor(waste.dat$Time,
-           levels = c("6:30:00 PM", 
-                      "7:00:00 PM", 
-                      "7:30:00 PM",
-                      "8:00:00 PM",
-                      "8:30:00 PM",
-                      "9:00:00 PM",
-                      "9:30:00 PM",
-                      "10:00:00 PM",
-                      "10:30:00 PM",
-                      "11:00:00 PM",
-                      "11:30:00 PM"))
-
-# take a look at the data
-time.by.location.count=waste.dat %>%
-  group_by(Time) %>%
-  count(Location)%>%
-  write_tsv(path = paste0(out.dir,"time_by_location.txt"))
-
-# missing 7:30 at location 1 (24 observations): Added 3/6/2019 by DJL
-# missing 8:00 at location 3 (24 observations): Added 3/6/2019 by DJL
-# missing 10:30 at location 3 (24 observations): Still missing as of 3/6/2019
-# 
+# run 1 vs run 2
+df=water%>%
+  filter(run==1)%>
+  
+  # Interleaved histograms
+ggplot(water, aes(x=value , color=run)) +
+  geom_histogram(fill="white", position="dodge")+
+  theme(legend.position="top")
+# Add mean lines
+p<-ggplot(df, aes(x=weight, color=sex)) +
+  geom_histogram(fill="white", position="dodge")+
+  geom_vline(data=mu, aes(xintercept=grp.mean, color=sex),
+             linetype="dashed")+
+  theme(legend.position="top")
+p
+  
