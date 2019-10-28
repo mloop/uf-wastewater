07dc6a5621a49d6c85953799ee85048fafe7a7b3
Matthew Loop <matthew.shane.loop@icloud.com>
2019-10-27 20:58:14 -0400

    Old updates

--- reports/acs-presentation/observed-metabolite-time-series.R
+++ reports/acs-presentation/observed-metabolite-time-series.R
@@ -1,8 +1,14 @@
 library(tidyverse)
 
-water <- read_tsv("../../data/water_cleaned.txt")
+water <- read_tsv("../../data/water_cleaned.txt") %>%
+  mutate(has_value = if_else(is.na(value) == TRUE, 0, 1),
+         time_pretty = as.character(time_pretty)) %>%
+  group_by(metabolite) %>%
+  mutate(total_values = sum(has_value)) %>%
+  filter(total_values > 6)
 
 p <- water %>%
+<<<<<<< Updated upstream
   mutate(has_value = if_else(is.na(value) == TRUE, 0, 1)) %>%
   group_by(metabolite) %>%
   mutate(total_values = sum(has_value)) %>%
@@ -11,17 +17,25 @@ p <- water %>%
   ggplot(aes(x = time_pretty, y = value, color = metabolite, linetype = extraction, group = interaction(extraction, metabolite))) +
   geom_path() +
   facet_grid(machine ~location) +
+=======
+  ungroup() %>%
+  group_by(time_pretty, metabolite, location) %>%
+  summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+  ggplot(aes(x = time_pretty, y = mean_value, color = metabolite, group = metabolite)) +
+  geom_path() +
+  facet_wrap(~location) +
+  theme_bw() +
+>>>>>>> Stashed changes
   theme(
-    legend.position = "bottom"
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 45, hjust = 1)
   ) +
   scale_color_viridis_d(name = "") +
   labs(
-    title = "Observed concentrations of each metabolite over time, by run and location",
-    subtitle = "Values are limited to those less than 100,000. Metabolites limited to those with at least 7 non-missing values.",
-    y = "Concentration (ng/mL)"
+    title = "Observed concentrations of each metabolite over time, by location",
+    subtitle = "Values are averaged over extractions and machine runs. Metabolites limited to those with at least 7 non-missing values.",
+    y = "Concentration (ng/mL)",
+    x = "Time"
   )
 
-ggsave("observed-metabolite-time-series.png", p, dpi = 600,
-       width = 10,
-       height = 5,
-       units = "in")
+ggsave("observed-metabolite-time-series.png", p, dpi = 600)
