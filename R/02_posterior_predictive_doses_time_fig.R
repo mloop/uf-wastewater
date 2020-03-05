1be47fa59150e2df7b986b75343a8574dc042098
Matthew Loop <matthew.shane.loop@icloud.com>
2020-03-05 13:15:48 -0700

    Use concentration and mass load in appropriate places

--- R/02_posterior_predictive_doses_time_fig.R
+++ R/02_posterior_predictive_doses_time_fig.R
@@ -2,13 +2,15 @@ library(tidyverse)
 library(brms)
 library(cowplot)
 
-readRDS("../output/02_posterior_predictive_doses_stadium.rds") %>%
+predicted_consumption <- readRDS("../output/02_posterior_predictive_doses_stadium.rds") %>%
   filter(consumption_missing_stadium == 0) %>%
   group_by(metabolite, time_pretty) %>%
   summarise(median_mass_load = quantile(mass_load_stadium, probs = 0.5, na.rm = FALSE),
             low_mass_load = quantile(mass_load_stadium, probs = 0.25, na.rm = FALSE),
             high_mass_load = quantile(mass_load_stadium, probs = 0.75, na.rm = FALSE),
-  ) %>%  
+  )
+
+predicted_consumption %>%
 ggplot(aes(x = time_pretty, y = median_mass_load)) +
   geom_point(size = 0.3) +
   geom_path(aes(group = metabolite), size = 0.3) +
@@ -23,7 +25,7 @@ ggplot(aes(x = time_pretty, y = median_mass_load)) +
   labs(
     y = "Predicted mass load (mg)",
     x = "Time of collection",
-    title = "Median predicted mass load for each compound\nover previous 30 minutes"
+    title = "Median estimated mass load (mg) for each\nsubstance through system over previous\n30 minutes"
   ) -> p
 
 ggsave(file = "../figs/02_posterior_predictive_doses_time.png", p, width = 5, height = 5, units = "in")
