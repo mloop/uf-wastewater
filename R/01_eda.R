48cf83f123fb13b302d6f99a8a1454f20a3a4ec6
Xinsong Du <xinsongdu@Xinsongs-MacBook-Pro.local>
2020-08-26 17:09:52 -0400

    add column number adjustment to histogram

--- R/01_eda.R
+++ R/01_eda.R
@@ -126,7 +126,7 @@ water_analysis %>%
   geom_vline(aes(xintercept = lloq), linetype = "dashed", color = "green") +
   geom_vline(aes(xintercept = uloq), linetype = "dashed", color = "red") +
   theme_bw() +
-  facet_wrap(~ metabolite, scales = "free") +
+  facet_wrap(~ metabolite, scales = "free", ncol = 2) +
   labs(x = "Measured concentration (ng/mL)") -> p
 ggsave(filename = "../figs/01_histogram_top_metabolites.png", p, width = 7, height = 5, units = "in")
 
